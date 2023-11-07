71d4dd3908cee7e3b5d579817f09e4f97d85c7e8
Victor Arruda Magalhaes <victor.arruda.magalhaes@gmail.com>
2023-11-07 17:03:48 -0300

    feat: ajusta nome do estado

--- app/checkout/FormSteps/serviceData.js
+++ app/checkout/FormSteps/serviceData.js
@@ -28,9 +28,9 @@ export default function ServiceData({ product, products, setService }) {
       .then(({ data: { data: response } }) => {
         const { Filiais: stories } = response;
         const initialStates = [];
-        stories.map(({ estado }) => {
+        stories.map(({ estado, estado_nome }) => {
           const node = {
-            estado: estado,
+            estado: estado_nome,
             uf: estado.toLowerCase()
           };
           initialStates.push(node);
