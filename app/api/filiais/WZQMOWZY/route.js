02c452f65d80c1c5fc4691f0cba6d110567ea86e
Victor Arruda Magalhaes <victor.arruda.magalhaes@gmail.com>
2023-12-08 10:09:27 -0300

    feat: cria as requisicoes necessarias

---
+++ app/api/filiais/WZQMOWZY/route.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-undef */
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API}/comercial/scd/filiais/WZQMOWZY`,
+    {
+      headers: {
+        'Content-Type': 'application/json',
+        'mz-integration': 'sempre'
+      }
+    }
+  );
+
+  const data = await res.json();
+
+  return NextResponse.json({ data });
+}
