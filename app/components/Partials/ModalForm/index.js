966c1b142d2d6649f439d837a4be1d51bc76dae8
Victor Arruda Magalhaes <victor.arruda.magalhaes@gmail.com>
2024-03-15 14:49:09 -0300

    feat: adiciona url no body do form

--- app/components/Partials/ModalForm/index.js
+++ app/components/Partials/ModalForm/index.js
@@ -67,7 +67,8 @@ export default function ModalForm({ open, onClose }) {
                 contato_nome: values.name,
                 contato_email: values.mail,
                 contato_telefone: values.phone,
-                contato_origem: 'site_st'
+                contato_origem: 'site_st',
+                formulario_origem: window.location.href
               });
 
               axios
